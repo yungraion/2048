efc5e856dd81d697597c0c53ab5067e1868c05c9
raion <leoneberwein@gmx.de>
2020-02-07 15:20:14 +0100

    finished game

--- app/src/main/java/com/example/a2048/MainActivity.kt
+++ app/src/main/java/com/example/a2048/MainActivity.kt
@@ -3,50 +3,105 @@ package com.example.a2048
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.MotionEvent
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
 
+
     var buttonArray = emptyArray<Array<Button>>()
+    var counter: Int = 0
+    var score = 0
+    var best = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView6.visibility = View.INVISIBLE
         buttonArray = arrayOf(
-            arrayOf(button0,button1,button2,button3),
-            arrayOf(button4,button5,button6,button7),
-            arrayOf(button8,button9,button10,button11),
-            arrayOf(button12,button13,button14,button15))
+            arrayOf(button0, button1, button2, button3),
+            arrayOf(button4, button5, button6, button7),
+            arrayOf(button8, button9, button10, button11),
+            arrayOf(button12, button13, button14, button15)
+        )
+
         Log.println(Log.WARN, "DEBUG", "START")
 
-        GridLayout1.setOnTouchListener(object : OnSwipeTouchListener(this) {
-            override fun onSwipeLeft() {
-                Log.println(Log.WARN,"DEBUG", "left")
-                swipeRight()
+
+    }
+
+    fun moveTiles(startX: Int, startY: Int, targetX: Int, targetY: Int) {
+        if (buttonArray[targetX][targetY].text.equals("")) {
+            buttonArray[targetX][targetY].text= buttonArray[startX][startY].text.toString()
+            buttonArray[startX][startY].text=("")
+        } else if (buttonArray[startX][startY].text.equals(buttonArray[targetX][targetY].text)) {
+            var temp: Int = buttonArray[targetX][targetY].text.toString().toInt()
+            buttonArray[targetX][targetY].text=("" + temp * 2)
+            buttonArray[startX][startY].text=("")
+            counter--
+
+//           update score
+            score += temp * 2
+            textView4.text=score.toString()
+        }
+
+    }
+
+    fun right(view: View) {
+        for (line in 0..3) {
+            for (btn in 0..2) {
+                moveTiles(line, btn, line, btn + 1)
             }
+        }
+        createNewTile()
 
-            override fun onSwipeRight() {
-                Log.println(Log.WARN,"DEBUG", "right")
-                swipeLeft()
+    }
+
+    fun left(view: View) {
+        for (line in 0..3) {
+            for (btn in 0..2) {
+                var temp = 3 - btn
+                moveTiles(line, temp, line, temp - 1)
             }
-        })
+        }
+        createNewTile()
     }
 
-    fun swipeLeft() {
-        Toast.makeText(this, "Left", Toast.LENGTH_LONG).show()
-
+    fun down(view: View) {
+        for (btn in 0..3) {
+            for (line in 0..2) {
+                moveTiles(line, btn, line + 1, btn)
+            }
+        }
+        createNewTile()
     }
 
-    fun swipeRight() {
-        Toast.makeText(this, "Right", Toast.LENGTH_LONG).show()
+    fun up(view: View) {
+        for (btn in 0..3) {
+            for (line in 0..2) {
+                var temp = 3 - line
+                moveTiles(temp, btn, temp - 1, btn)
+            }
+        }
+        createNewTile()
     }
 
-    fun play(v: View) {
+
+    fun play(view: View) {
+        if(score > best) {
+            best = score
+        }
+        textView3.text=best.toString()
+
+        score = 0
+        textView4.text=score.toString()
+
+        textView6.visibility = View.INVISIBLE
         button16.text = "Restart"
+        counter = 2
 
         for (row in 0..3) {
             for (column in 0..3) {
@@ -62,16 +117,38 @@ class MainActivity : AppCompatActivity() {
         var randomRow2 = (Math.random() * 4).toInt()
         var randomColumn2 = (Math.random() * 4).toInt()
 
-        while(randomRow == randomRow2 && randomColumn == randomColumn2) {
+        while (randomRow == randomRow2 && randomColumn == randomColumn2) {
             randomRow2 = (Math.random() * 4).toInt()
             randomColumn2 = (Math.random() * 4).toInt()
         }
 
         buttonArray[randomRow2][randomColumn2].text = "2"
-
-
-
-
-
     }
+
+    private fun createNewTile() {
+        if (counter < 16) {
+            var temp = true
+            counter++
+                while(temp){
+                    //create random position
+                    val rowRandom = (Math.random() * 4).toInt()
+                    val columnRandom = (Math.random() * 4).toInt()
+
+                    //check if position is occupied
+                    if(buttonArray[rowRandom][columnRandom].text.toString().equals("")){
+                        //create tile
+                        buttonArray[rowRandom][columnRandom].text = "2"
+                        temp = false
+                    }
+            }
+        } else {
+            //lost
+            textView6.visibility = View.VISIBLE
+
+
+
+
+        }
+    }
+
 }
--- app/src/main/java/com/example/a2048/OnSwipeTouchListener.kt
+++ app/src/main/java/com/example/a2048/OnSwipeTouchListener.kt
@@ -1,58 +1,74 @@
-package com.example.a2048
-
 import android.content.Context
-import android.util.Log
 import android.view.GestureDetector
 import android.view.GestureDetector.SimpleOnGestureListener
 import android.view.MotionEvent
 import android.view.View
 import android.view.View.OnTouchListener
 
+//open class OnSwipeTouchListener : View.OnTouchListener {
+//
+//    private val gestureDetector = GestureDetector(GestureListener())
+//
+//    fun onTouch(event: MotionEvent): Boolean {
+//        return gestureDetector.onTouchEvent(event)
+//    }
+//
+//    private inner class GestureListener : GestureDetector.SimpleOnGestureListener() {
+//
+//        private val SWIPE_THRESHOLD = 100
+//        private val SWIPE_VELOCITY_THRESHOLD = 100
+//
+//        override fun onDown(e: MotionEvent): Boolean {
+//            return true
+//        }
+//
+//        override fun onSingleTapConfirmed(e: MotionEvent): Boolean {
+//            onTouch(e)
+//            return true
+//        }
+//
+//
+//        override fun onFling(
+//            e1: MotionEvent,
+//            e2: MotionEvent,
+//            velocityX: Float,
+//            velocityY: Float
+//        ): Boolean {
+//            val result = false
+//            try {
+//                val diffY = e2.y - e1.y
+//                val diffX = e2.x - e1.x
+//                if (Math.abs(diffX) > Math.abs(diffY)) {
+//                    if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+//                        if (diffX > 0) {
+//                            onSwipeRight()
+//                        } else {
+//                            onSwipeLeft()
+//                        }
+//                    }
+//                } else {
+//                    // onTouch(e);
+//                }
+//            } catch (exception: Exception) {
+//                exception.printStackTrace()
+//            }
+//
+//            return result
+//        }
+//    }
+//
+//    override fun onTouch(v: View, event: MotionEvent): Boolean {
+//        return gestureDetector.onTouchEvent(event)
+//    }
+//
+//    open fun onSwipeRight() {}
+//
+//    open fun onSwipeLeft() {}
+//
+//    open fun onSwipeTop() {}
+//
+//    open fun onSwipeBottom() {}
+//
+//}
 
-/**
- * Detects left and right swipes across a view.
- */
-abstract class OnSwipeTouchListener(context: Context?) : OnTouchListener {
-    private val gestureDetector: GestureDetector
-    abstract fun onSwipeLeft()
-    abstract fun onSwipeRight()
-    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-        Log.println(Log.WARN,"DEBUG", "touch")
-        return gestureDetector.onTouchEvent(event)
-    }
 
-    private inner class GestureListener : SimpleOnGestureListener() {
-        override fun onDown(e: MotionEvent): Boolean {
-            return true
-        }
-
-        override fun onFling(
-            e1: MotionEvent,
-            e2: MotionEvent,
-            velocityX: Float,
-            velocityY: Float
-        ): Boolean {
-            val distanceX = e2.x - e1.x
-            val distanceY = e2.y - e1.y
-            Log.println(Log.WARN,"DEBUG", "")
-            if (Math.abs(distanceX) > Math.abs(distanceY) && Math.abs(
-                    distanceX
-                ) > SWIPE_DISTANCE_THRESHOLD && Math.abs(
-                    velocityX
-                ) > SWIPE_VELOCITY_THRESHOLD
-            ) {
-                if (distanceX > 0) onSwipeRight() else onSwipeLeft()
-                Log.println(Log.WARN,"DEBUG", distanceX.toString())
-                return true
-            }
-            return false
-        }
-
-        private val SWIPE_DISTANCE_THRESHOLD = 100
-        private val SWIPE_VELOCITY_THRESHOLD = 100
-    }
-
-    init {
-        gestureDetector = GestureDetector(context, GestureListener())
-    }
-}
